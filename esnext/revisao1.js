68a4e8f3c286c701ad24fd1503d2cbe5755b7cc3
diegoklein <dgzklein1@hotmail.com>
2021-01-11 04:34:19 -0300

    revisao 1 e 2

---
+++ esnext/revisao1.js
@@ -0,0 +1,22 @@
+{
+    var a = 2 //Escopo Global
+    let b = 3 //Escopo de Bloco
+
+    console.log(b)
+}
+
+console.log(a)
+
+//Template String
+const produto = 'iPad'
+console.log(`${produto} e caro!`)
+
+//Destructuring
+const [l, e, ...tras] = 'Cod3r'
+console.log(l,e,tras)
+
+const [x,y] = [1,2,3]
+console.log(x,y)
+
+const {idade: i, nome } = { nome: 'Ana', idade: 9 }
+console.log(i, nome)
